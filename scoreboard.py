ac84fb4a43e65c0122c4874d9ee98d44b611fd28
Me45y63 <66312028+Me45y63@users.noreply.github.com>
2021-07-20 00:11:25 +0000

    added py files
    A simple snake game created using python and the Turtle module

---
+++ scoreboard.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 16, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
+        self.color("white")
+        self.penup()
+        self.goto(x=0, y=260)
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        with open("data.txt", mode="w") as file:
+            file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
